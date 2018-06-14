36a765c2d975106711c83a81c518ec1feb11283b
Christopher Eliasson <ChristopherEliasson@gmail.com>
2018-06-14 18:27:29 +0200

    - Reworked project to Framework

---
+++ CACToolkit/CACToolkit/CACToolkit.h
@@ -0,0 +1,19 @@
+//
+//  CACToolkit.h
+//  CACToolkit
+//
+//  Created by Christopher Eliasson on 2018-06-14.
+//  Copyright © 2018 Code and Crayons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CACToolkit.
+FOUNDATION_EXPORT double CACToolkitVersionNumber;
+
+//! Project version string for CACToolkit.
+FOUNDATION_EXPORT const unsigned char CACToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CACToolkit/PublicHeader.h>
+
+
