61a8e013bc9171310436964333954aea642d88b5
Christopher Eliasson <ChristopherEliasson@gmail.com>
2018-06-14 18:08:41 +0200

    - Initiated project

---
+++ CACToolkit/CACToolkit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CACToolkit
+//
+//  Created by Christopher Eliasson on 2018-06-14.
+//  Copyright © 2018 Code and Crayons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
