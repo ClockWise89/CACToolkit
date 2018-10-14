d9c346aa1ddccda810953a03805d680205d055e3
Christopher Eliasson <ChristopherEliasson@gmail.com>
2018-10-14 21:10:38 +0200

    - Working on tests

--- CACToolkitTests/CACQueueTests.m
+++ CACToolkitTests/CACQueueTests.m
@@ -27,20 +27,23 @@
     [_queue cancelAllOperations];
 }
 
-- (void)testFIFO {
+- (void)testLIFO {
     XCTestExpectation *expectation = [self expectationWithDescription:@"FIFO Queue"];
-    __block NSMutableArray *result = [NSMutableArray new];
+    __block NSMutableDictionary *result = [NSMutableDictionary new];
     
     _queue = [[Queue alloc] init:QueueLIFO];
     int limit = 15;
-    for (int i=0;i<limit;i++) {
+    __block int done = 1;
+    for (int i=1;i<limit;i++) {
+        
+        __block NSString *added = [NSString stringWithFormat:@"%i", i];
         [_queue executeOperation:^{
-    
-            [NSThread sleepForTimeInterval:0.3f];
+            [NSThread sleepForTimeInterval:0.001f];
             NSLog(@"Operation %i is done.", i);
-            [result addObject: [NSString stringWithFormat:@"%i", i]];
-            
-            
+    
+            NSString *finished = [NSString stringWithFormat:@"%i", done];
+            done++;
+            [result setObject:finished forKey:added];
             
         } key:[NSString stringWithFormat:@"%i", i] cancelExisting:YES withCallback:^{
             [expectation fulfill];
@@ -50,11 +53,22 @@
     [self waitForExpectationsWithTimeout:10 handler:^(NSError *error) {
         NSLog(@"Finishing result: %@", result);
         
+        // Test is successful if all but the first operation is LIFO (the first operation will start before the queue is fully loaded and
+        // won't cancel that operation. Thus the first operation finished will be the first one added in this case.
+        __block BOOL success = YES;
+        [result enumerateKeysAndObjectsUsingBlock:^(NSString *added, NSString *finished, BOOL *stop) {
+            
+            if ((limit - [added intValue] + 1) != [finished intValue] && [added intValue] != 1) {
+                success = NO;
+            }
+        }];
+        
+        XCTAssert(success, @"Should be true");
     }];
 }
 
-- (void)testLIFO {
-    _queue = [[Queue alloc] init:QueueLIFO];
+- (void)testFIFO {
+    _queue = [[Queue alloc] init:QueueFIFO];
 }
 
 - (void)testCancelAllOperations {
